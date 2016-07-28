ca37e3174e11dda51f6a43ea2698c9d69ce0e653
Gianluca Petrillo <petrillo@fnal.gov>
2016-07-27 22:21:54 -0500

    Added lar::providersFrom() to ServiceUtil.h
    This function allows one-step creation of a service provider pack, making the initialisation of a provider shorter.

--- test/CoreUtils/ServiceUtil_test.cc
+++ test/CoreUtils/ServiceUtil_test.cc
@@ -37,17 +37,27 @@
 //------------------------------------------------------------------------------
 struct MyProvider: protected lar::UncopiableAndUnmovableClass {};
 
-class MyService {
-   MyProvider* prov = nullptr;
+struct MyOtherProvider: protected lar::UncopiableAndUnmovableClass {};
+
+struct YetAnotherProvider: protected lar::UncopiableAndUnmovableClass {};
+
+template <typename Provider>
+class MyServiceTemplate {
+   Provider* prov = nullptr;
 
       public:
-   MyService(MyProvider* ptr = nullptr): prov(ptr) {}
+   MyServiceTemplate(Provider* ptr = nullptr): prov(ptr) {}
 
-   using provider_type = MyProvider;
+   using provider_type = Provider;
 
    provider_type const* provider() const { return prov; }
   
-}; // MyService
+}; // MyServiceTemplate
+
+using MyService = MyServiceTemplate<MyProvider>;
+using MyOtherService = MyServiceTemplate<MyOtherProvider>;
+using YetAnotherService = MyServiceTemplate<YetAnotherProvider>;
+
 
 namespace lar {
    namespace details {
@@ -60,13 +70,24 @@ namespace lar {
 } // namespace lar
 
 
-
-
 // This is art. Well, kind of.
 struct GlobalServicesClass {
-   std::unique_ptr<MyService> myServicePtr = nullptr;
+   std::unique_ptr<MyService> myServicePtr;
+   std::unique_ptr<MyOtherService> myOtherServicePtr;
+   std::unique_ptr<YetAnotherService> yetAnotherServicePtr;
 }; // GlobalServicesClass
 GlobalServicesClass GlobalServices;
+
+
+template <typename Service>
+class ServiceHandleBase {
+      public:
+   Service* operator->() const { return instance; }
+   Service& operator*() const { return *instance; }
+      protected:
+   Service* instance = nullptr;
+}; // ServiceHandleBase<>
+
 
 namespace art {
    
@@ -76,18 +97,39 @@ namespace art {
          static constexpr art::ServiceScope scope_val
            = art::ServiceScope::LEGACY;
       };
+      template <>
+      struct ServiceHelper<MyOtherService> {
+         static constexpr art::ServiceScope scope_val
+           = art::ServiceScope::LEGACY;
+      };
+      template <>
+      struct ServiceHelper<YetAnotherService> {
+         static constexpr art::ServiceScope scope_val
+           = art::ServiceScope::LEGACY;
+      };
+
    } // namespace detail
    
-   template <>
-   class ServiceHandle<MyService, art::ServiceScope::LEGACY> {
-      MyService* instance;
-         public:
-      ServiceHandle(): instance(GlobalServices.myServicePtr.get()) {}
-      MyService* operator->() const { return instance; }
-      MyService& operator*() const { return *instance; }
-   }; // ServiceHandle<MyService>
    
+   template <>
+   struct ServiceHandle<MyService, art::ServiceScope::LEGACY>
+      : ::ServiceHandleBase<MyService>
+   { ServiceHandle() { instance = GlobalServices.myServicePtr.get(); } };
+   
+   template <>
+   struct ServiceHandle<MyOtherService, art::ServiceScope::LEGACY>
+      : ::ServiceHandleBase<MyOtherService>
+   { ServiceHandle() { instance = GlobalServices.myOtherServicePtr.get(); } };
+
+   template <>
+   struct ServiceHandle<YetAnotherService, art::ServiceScope::LEGACY>
+      : ::ServiceHandleBase<YetAnotherService>
+   { ServiceHandle() { instance = GlobalServices.yetAnotherServicePtr.get(); } };
+
 } // namespace art
+
+
+
 
 
 BOOST_AUTO_TEST_CASE(providerFromTest) {
@@ -108,7 +150,53 @@ BOOST_AUTO_TEST_CASE(providerFromTest) {
    // that's enough; let's clean up
    GlobalServices.myServicePtr.reset();
    
-} // BOOST_AUTO_TEST_CASE(larProviderFromTest)
+} // BOOST_AUTO_TEST_CASE(providerFromTest)
+
+
+
+BOOST_AUTO_TEST_CASE(providersFromTest) {
+   
+   // on the first try, there is no "other" service provider.
+   
+   MyProvider prov;
+   GlobalServices.myServicePtr = std::make_unique<MyService>(&prov);
+   GlobalServices.myOtherServicePtr = std::make_unique<MyOtherService>();
+   GlobalServices.yetAnotherServicePtr = std::make_unique<YetAnotherService>();
+   
+   auto provPack1 = lar::providersFrom<MyService>();
+   BOOST_CHECK_EQUAL(provPack1.get<MyProvider>(), &prov);
+   BOOST_CHECK_EXCEPTION(lar::providersFrom<MyOtherService>(), art::Exception,
+     [](art::Exception const& e)
+       { return e.categoryCode() == art::errors::NotFound; }
+     );
+   BOOST_CHECK_EXCEPTION(
+     (lar::providersFrom<MyService, MyOtherService>()), art::Exception,
+     [](art::Exception const& e)
+       { return e.categoryCode() == art::errors::NotFound; }
+     );
+   
+   // now let's create a "real" provider
+   MyOtherProvider oprov;
+   YetAnotherProvider yaprov;
+   GlobalServices.myOtherServicePtr = std::make_unique<MyOtherService>(&oprov);
+   GlobalServices.yetAnotherServicePtr
+     = std::make_unique<YetAnotherService>(&yaprov);
+     
+   auto provPack
+     = lar::providersFrom<MyService, MyOtherService, YetAnotherService>();
+   
+   // not using BOOST_CHECK_EQUAL because we can't stream ProviderPacks
+   BOOST_CHECK(provPack == lar::makeProviderPack(&prov, &oprov, &yaprov));
+   
+   BOOST_CHECK_EQUAL(lar::providerFrom<MyService>(), &prov);
+   BOOST_CHECK_EQUAL(lar::providerFrom<MyOtherService>(), &oprov);
+   BOOST_CHECK_EQUAL(lar::providerFrom<YetAnotherService>(), &yaprov);
+   
+   
+   // that's enough; let's clean up
+   GlobalServices.myServicePtr.reset();
+   
+} // BOOST_AUTO_TEST_CASE(providersFromTest)
 
 
 
